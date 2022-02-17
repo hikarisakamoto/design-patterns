2a367a26a70428a56bc2bd4b2eecc41f419ef90b
Sakamoto <hikarisakamoto@gmail.com>
2022-02-17 15:37:30 -0300

    Builder patterns starting up

---
+++ BuilderPatterns/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var hello = "Hello";
+        var stringBuilder = new StringBuilder();
+        stringBuilder.Append("<p>");
+        stringBuilder.Append(hello);
+        stringBuilder.Append("</p>");
+        Console.WriteLine(stringBuilder.ToString());
+
+        var words = new[] { "Hello", "world" };
+        stringBuilder.Clear();
+        stringBuilder.Append("<ul>");
+        foreach (var word in words)
+        {
+            stringBuilder.AppendFormat("<li>{0}</li>", word);
+        }
+        stringBuilder.Append("</ul>");
+        Console.WriteLine(stringBuilder.ToString());
+    }
+}
